a037adac0c68bc0d1e5a616651e7bf100d9ede9f
Platon <platon.tikhnenko@gmail.com>
2025-11-07 19:08:05 -0800

    registered join community endpoint

--- MainService/community/urls.py
+++ MainService/community/urls.py
@@ -3,5 +3,6 @@ from . import routes
 
 urlpatterns = [
     path('find', routes.find_community),
-    path('create', routes.create_community)
+    path('create', routes.create_community),
+    path('join', routes.join_community)
 ]
